0ee30f60144b913e7596dd4368cb418c98057f48
Ben Finlay <benjamin.finlay@gmail.com>
2022-04-12 21:17:07 -0400

    removed the function wrapper to try and shorten the code.  Got it to 7 lines.

--- spinner2.js
+++ spinner2.js
@@ -1,13 +1,7 @@
-const spinner = function() {
-  let timer = 100;
-  let spinArr = ['\r|   ','\r/   ','\r-   ', '\r\\   ', '\r|   ' ];
-
-  for (const spin of spinArr) {
-    setTimeout(() => {
-      process.stdout.write(spin);
-    }, timer);
-    timer = timer + 200;
-  }
+let timer = 100, spinArr = ['\r|   ','\r/   ','\r-   ', '\r\\   ', '\r|   ','\r/   ','\r-   ', '\r\\   ', '\r|   '];
+for (const spin of spinArr) {
+  setTimeout(() => {
+    process.stdout.write(spin);
+  }, timer);
+  timer = timer + 200;
 };
-
-spinner();
